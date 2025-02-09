2e8202f8989615d6ff05a7cf6066e61ad703bb94
Jakub Starostecki <jakub.starostecki27@gmail.com>
2025-02-09 19:30:00 +0100

    maintenance update

--- reddit_proxy.py
+++ reddit_proxy.py
@@ -30,7 +30,7 @@ ydl_opts = {
     'extract_flat': True,
     'restrictfilenames': True,
     'noplaylist': True,
-    'outtmpl': directory+"%(title)s.%(ext)s"
+    'outtmpl': directory+"%(id)s.%(ext)s"
 }
 
 # Remove cached files on boot
@@ -63,7 +63,7 @@ def video(path):
     try:
         r = requests.get(url=path,cookies=cookies,headers=headers)
         soup = BeautifulSoup(r.text, features="html.parser")
-        info = json.loads(soup.find("shreddit-player")["packaged-media-json"])["playbackMp4s"]["permutations"]
+        info = json.loads(soup.find("shreddit-player-2")["packaged-media-json"])["playbackMp4s"]["permutations"]
         url = info[len(info)-1]["source"]["url"]
     # Fallback to a video without sound
     except (TypeError, KeyError):
@@ -75,46 +75,50 @@ def video(path):
         r = requests.get(url=json_path,cookies=cookies,headers=headers)
         info = json.loads(r.text)[0]["data"]["children"][0]["data"]
         try:
+            name = info["url"].replace("https://v.redd.it/","")
             url = info["media"]["reddit_video"]["fallback_url"]
-            name = info["id"]
             # If it's not a gif we can try and combine the audio and video ourselves
             if combine_audio_video and not info["media"]["reddit_video"]["is_gif"]:
-                if encoding:
-                    audio_url = info["url"]
-                    audio_url = audio_url + "/DASH_AUDIO_"
-                    r = requests.get(url=info["media"]["reddit_video"]["hls_url"],cookies=cookies,headers=headers)
-                    best_hls = ""
-                    for line in r.text.splitlines():
-                        if "#EXT-X-MEDIA" in line:
-                            hls = re.search('HLS_AUDIO_(.*).m3u8',line)
-                            best_hls = hls.group(1)
-                    audio_url = audio_url + best_hls + ".mp4"
-                    if not os.path.exists(directory+name+".mp4"):
+                if not os.path.exists(directory+name+".mp4"):
+                    if encoding:
+                        audio_url = info["url"]
+                        audio_url = audio_url + "/DASH_AUDIO_"
+                        r = requests.get(url=info["media"]["reddit_video"]["hls_url"],cookies=cookies,headers=headers)
+                        best_hls = ""
+                        for line in r.text.splitlines():
+                            if "#EXT-X-MEDIA" in line:
+                                hls = re.search('HLS_AUDIO_(.*).m3u8',line)
+                                best_hls = hls.group(1)
+                        audio_url = audio_url + best_hls + ".mp4"
                         audio = ffmpeg.input(audio_url)
                         video = ffmpeg.input(url)
                         try:
-                            ffmpeg.output(audio, video, directory+name+".mp4", format="mp4", vcodec="libx264", acodec="copy", crf=27, preset="veryfast").run(overwrite_output=True)
+                            ffmpeg.output(audio, video, directory+name+".mp4", format="mp4", vcodec="copy", acodec="copy", crf=27, preset="veryfast").run(overwrite_output=True)
                         except ffmpeg.Error:
                             return redirect(url, code=302)
+                        file = open(directory+name+".mp4", "rb")
+                        returnable_result = io.BytesIO(file.read())
+                        file.close()
+                        return send_file(path_or_file=returnable_result,download_name="reddit_video.mp4")
+                    # In case of disabled encoding utilize yt-dlp
+                    else:
+                        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                            info = ydl.extract_info(path, download=False)
+                            title = ydl.prepare_filename(info)
+                            ydl.download(path)
+                        try:
+                            file = open(title, "rb")
+                            returnable_result = io.BytesIO(file.read())
+                            file.close()
+                            return send_file(path_or_file=returnable_result,download_name="reddit_video.mp4")
+                        # Reddit blocked us so proceed with the file without audio
+                        except FileNotFoundError:
+                            pass
+                else:
                     file = open(directory+name+".mp4", "rb")
                     returnable_result = io.BytesIO(file.read())
                     file.close()
                     return send_file(path_or_file=returnable_result,download_name="reddit_video.mp4")
-                # In case of disabled encoding utilize yt-dlp
-                else:
-                    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                        info = ydl.extract_info(path, download=False)
-                        title = ydl.prepare_filename(info)
-                        if not os.path.exists(title):
-                            ydl.download(path)
-                    try:
-                        file = open(title, "rb")
-                        returnable_result = io.BytesIO(file.read())
-                        file.close()
-                        return send_file(path_or_file=returnable_result,download_name="reddit_video.mp4")
-                    # Reddit blocked us so proceed with the file without audio
-                    except FileNotFoundError:
-                        pass
         except (TypeError, KeyError):
             try:
                 url = info["preview"]["reddit_video_preview"]["fallback_url"]
@@ -145,6 +149,7 @@ def embed(path):
         json_path = path + ".json"
     r = requests.get(url=json_path,cookies=cookies,headers=headers)
     info = json.loads(r.text)[0]["data"]["children"][0]["data"]
+    thumbnail = info["thumbnail"].replace("amp;","")
     name = info["subreddit_name_prefixed"]
     title = info["title"]
     try:
@@ -161,7 +166,7 @@ def embed(path):
             except (TypeError, KeyError):
                 return '<head><meta http-equiv="refresh" content="0; url='+path+'"></head>'
     if not ".gif" in info["url"][-4:] and not ".jpeg" in info["url"][-5:] and not ".jpg" in info["url"][-4:] and not ".png" in info["url"][-4:]:
-        return '<head><meta name="theme-color" content="#FF4500"><meta http-equiv="refresh" content="0; url='+path+'"><meta property="og:title" content="'+name+' - '+title+'"><meta property="og:url" content="'+path+'"><meta property="og:video" content="http://'+str(request.host)+'/video'+str(request.full_path)+'"><meta property="og:image" content="http://'+str(request.host)+'/video'+str(request.full_path)+'"><meta property="og:type" content="video"><meta property="og:video:type" content="video/mp4"><meta property="og:video:width" content="'+str(width)+'"><meta property="og:video:height" content="'+str(height)+'"></head>'
+        return '<head><meta name="theme-color" content="#FF4500"><meta http-equiv="refresh" content="0; url='+path+'"><meta property="og:title" content="'+name+' - '+title+'"><meta property="og:url" content="'+path+'"><meta property="og:video" content="http://'+str(request.host)+'/video/'+path+'"><meta property="og:image" content="'+thumbnail+'"><meta property="og:type" content="video"><meta property="og:video:type" content="video/mp4"><meta property="og:video:width" content="'+str(width)+'"><meta property="og:video:height" content="'+str(height)+'"></head>'
     else:
         return '<head><meta name="theme-color" content="#FF4500"><meta http-equiv="refresh" content="0; url='+path+'"><meta property="og:title" content="'+name+' - '+title+'"><meta property="og:url" content="'+path+'"><meta property="og:image" content="'+info["url"]+'"><meta property="og:type" content="image"><meta property="og:image:type" content="image/gif"><meta property="og:image:width" content="'+str(width)+'"><meta property="og:image:height" content="'+str(height)+'"><meta name="twitter:card" content="summary_large_image"><meta name="twitter:image:src" content="'+info["url"]+'"></head>'
 
